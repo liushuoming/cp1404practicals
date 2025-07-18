358463b56353289e92072582bce4196f036749a3
liushuoming <shuoming.liu@my.jcu.edu.au>
2025-07-18 13:24:02 +0800

    box layout

---
+++ prac_08/box_layout_demo.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+class BoxLayoutDemoApp(App):
+    def build(self):
+        self.root = Builder.load_file('box_layout.kv')
+        return self.root
+
+    def handle_greet(self):
+        name = self.root.ids.input_name.text
+        self.root.ids.output_label.text = f"Hello {name}"
+
+    def handle_clear(self):
+        self.root.ids.input_name.text = ''
+        self.root.ids.output_label.text = ''
+
+if __name__ == '__main__':
+    BoxLayoutDemoApp().run()
